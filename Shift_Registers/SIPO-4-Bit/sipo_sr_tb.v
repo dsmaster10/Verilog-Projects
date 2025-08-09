58e1fa5da2ea1707fee67efd3437f0eaf0302012
dsmaster10 <97398617+dsmaster10@users.noreply.github.com>
2025-08-10 01:36:58 +0530

    Add files via upload

---
+++ Shift_Registers/SIPO-4-Bit/sipo_sr_tb.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+module sipo_sr_tb;
+
+	// Inputs
+	reg clk;
+	reg data_in;
+	reg reset;
+
+	// Outputs
+	wire [3:0] q;
+
+	// Instantiate the Unit Under Test (UUT)
+	sipo_sr uut (
+		.clk(clk), 
+		.data_in(data_in), 
+		.reset(reset), 
+		.q(q)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		forever #5 clk=~clk;
+        
+		// Add stimulus here
+	end
+	initial begin
+		reset=1; data_in=0;
+		#12 reset=0;
+		#3 data_in=1;
+		#10 data_in=0;
+		#5 data_in=1;
+		#7 data_in=1;
+		#15 $stop;
+	end
+	initial begin
+        $monitor("Time=%0t | clk=%b | reset=%b | data_in=%b | q=%b", 
+                 $time, clk, reset, data_in, q);
+    end
+      
+endmodule
+
