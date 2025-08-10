7fbee0f65d3c02dfb45c8be6f7f06a86a5595d00
dsmaster10 <97398617+dsmaster10@users.noreply.github.com>
2025-08-10 16:34:07 +0530

    Add files via upload

---
+++ Shift_Registers/PISO-4-Bit/piso_sr.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+module piso_sr(
+input clk,
+input load,
+input reset,
+input shift_en,
+input [3:0] data_in,
+output serial_out
+);
+reg[3:0] shift_reg; //internal register for storage
+
+always @(posedge clk ) begin
+    if(reset)begin
+        shift_reg<=4'b0000; //only reset signal means output =
+    end
+    else if(load)begin
+        shift_reg<=data_in; // it means data loaded
+    end
+    else if(shift_en)begin
+        shift_reg  <= {1'b0, shift_reg[3:1]};  // right shift with 0 fill msb
+    end
+end 
+
+assign serial_out=reset?1'b0:shift_reg[0];
+endmodule
---
+++ Shift_Registers/PISO-4-Bit/piso_sr_tb.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+module piso_sr_tb;
+
+	// Inputs
+	reg clk;
+	reg load;
+	reg reset;
+	reg shift_en;
+	reg [3:0] data_in;
+
+	// Outputs
+	wire serial_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	piso_sr uut (
+		.clk(clk), 
+		.load(load), 
+		.reset(reset), 
+		.shift_en(shift_en), 
+		.data_in(data_in), 
+		.serial_out(serial_out)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		load = 0;
+		reset = 0;
+		shift_en = 0;
+		data_in = 0;
+		forever #5clk=~clk;  
+	end 
+		// Add stimulus here
+		initial begin
+	
+			$monitor($time, " clk=%b load=%b shift_en=%b data_in=%b serial_out=%b", clk, load, shift_en, data_in, serial_out);
+
+		//reset pulse
+			reset=1;
+			#10 reset=0; 
+			
+			// loading 
+			#10 load=1;
+			shift_en=0;
+			data_in=4'b1101;
+			
+			// shifting
+			#10 load=0;
+			shift_en=1;
+			
+	#100 $finish;
+
+	end
+      
+endmodule
+
